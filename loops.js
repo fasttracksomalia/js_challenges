be3dba136951640ca45b7752ce6b495eaa337ab0
= <=>
2023-10-07 12:32:37 +0100

    seperated the files

--- loops.js
+++ loops.js
@@ -1,48 +1,27 @@
+/*
+   this challenge file was created, so that I could write the code off my heart without using 
+   any code references, include Visual Studio Code snippets
+*/
+// how write a for loop that prints my name 10 times
 for (let i = 0; i < 10; i++) {
    console.log("Ahmed")
     
 }
 
+// how write a for while that prints my surname 10 times
 let count = 0;
-
 while (count < 10) {
     console.log("Dini")
     count++
 }
 
-let today = "Saturday"
-
-if (today == "Saturday") {
-    console.log("Come to the coding school")
-} else if (today == "Sunday") {
-    console.log("Sleep")
-} else if (today == "Monday") {
-    console.log("go to school")
-} else {
-    console.log("Do whatever you like")
-}
-
-
-switch (today) {
-    case "Saturday":
-        console.log("Coding time...")
-        break;
-    case "Sunday":
-        console.log("Sleeping time...")
-        break;
-    case "Monday":
-            console.log("School time...")
-            break;
-
-    default:
-        console.log("Free time")
-        break;
-}
 
 
 
 
+// write a do..while loop that prints the word 'Noor' 10 times
 do {
     console.log("Noor")
     count++;
 } while (count < 10);
+
