de4e744c3f9b64bce63eee2eae4c82bbdacca6e8
= <=>
2023-10-07 13:16:46 +0100

    added a function file

---
+++ functions.js
@@ -0,0 +1,7 @@
+// write a simple JavaScript function called multipy that takes two numbers as params and returns the result to the caller
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+let result = multiply(5,6)
+console.log(result)
